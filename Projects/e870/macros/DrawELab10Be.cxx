ac10b293ca3c3f3b777a30376c4d7857b967937f
GIRARD ALCINDOR Valérian <girard-alcindor@ijclab.in2p3.fr>
2023-12-05 17:19:25 +0100

    Updating e870

--- Projects/e870/macros/DrawELab10Be.cxx
+++ Projects/e870/macros/DrawELab10Be.cxx
@@ -1,28 +1,29 @@
 void DrawELab10Be() {
-  TCanvas *c1 = new TCanvas;
+  TCanvas* c1 = new TCanvas;
   c1->cd();
-  TFile* f3 =
-      new TFile("/Users/valerian/Software/nptool_gitlab/nptool/Outputs/Analysis/AnaMugastAtLise10Bepalpha.root");
+  TFile* f3 = new TFile("/Users/valerian/Software/nptool_gitlab/nptool/Outputs/Analysis/test.root");
+  // new TFile("/Users/valerian/Software/nptool_gitlab/nptool/Outputs/Analysis/AnaMugastAtLise10Bepalpha.root");
   TTree* t3 = (TTree*)f3->FindObjectAny("PhysicsTree");
   t3->Draw("ELab:ThetaLab>>hET10Bepalpha(1000,0,50,1000,0,500)", "CsI_E>0", "");
-  TH2F* hBe1 = (TH2F*) gDirectory->FindObject("hET10Bepalpha");
+  TH2F* hBe1 = (TH2F*)gDirectory->FindObject("hET10Bepalpha");
   hBe1->SetMarkerStyle(1);
   hBe1->SetMarkerColor(kRed);
   hBe1->Draw();
   NPL::Reaction r3("10Be(p,4He)7Li@360MeV");
   r3.GetKinematicLine3()->Draw("same");
   r3.GetKinematicLine4()->Draw("same");
-  
-  TFile * fa = new TFile("ha.root","open");
-  TH2F* ha = (TH2F*) fa->FindObjectAny("h");
-  ha->SetMarkerColor(kBlack);
-  TFile * fli = new TFile("hli.root","open");
-  TH2F* hli = (TH2F*) fli->FindObjectAny("h2");
-  hli->SetMarkerColor(kBlue);
 
-  ha->Draw("same");
-  hli->Draw("same");
+  // TFile* fa = new TFile("ha.root", "open");
+  // TH2F* ha = (TH2F*)fa->FindObjectAny("h");
+  // ha->SetMarkerColor(kBlack);
+  // TFile* fli = new TFile("hli.root", "open");
+  // TH2F* hli = (TH2F*)fli->FindObjectAny("h2");
+  // hli->SetMarkerColor(kBlue);
 
+  // ha->Draw("same");
+  // hli->Draw("same");
+
+  /*
   TCanvas *c2 = new TCanvas;
   c2->cd();
   TFile* f1 = new TFile("/Users/valerian/Software/nptool_gitlab/nptool/Outputs/Analysis/AnaMugastAtLise10Bed6Li.root");
@@ -43,4 +44,5 @@ void DrawELab10Be() {
 
   ha2->Draw("same");
   hli2->Draw("same");
+  */
 }
