9162bb5877dc2cb9e21a0df1c6489a96c74109e5
morfouace <pierre.morfouace@cea.fr>
2024-07-11 15:00:27 +0200

    updtatin FMPWPhysics for position lineratisation

--- NPLib/Detectors/PISTA/TFPMWPhysics.h
+++ NPLib/Detectors/PISTA/TFPMWPhysics.h
@@ -11,14 +11,14 @@
  * Original Author: P. Morfouace  contact address: pierre.morfouace@cea.fr   *
  *                                                                           *
  * Creation Date  : October 2023                                             *
- * Last update    :                                                          *
+ * Last update    : June 2024                                                *
  *---------------------------------------------------------------------------*
  * Decription:                                                               *
- *  This class hold FPMW Treated data                                *
+ *  This class hold FPMW Treated data                                        *
  *                                                                           *
  *---------------------------------------------------------------------------*
  * Comment:                                                                  *
- *                                                                           *   
+ * Don't forget  //!  On newly defined private variable                      *  
  *                                                                           *
  *****************************************************************************/
 
@@ -29,12 +29,15 @@
 #include <string>
 using namespace std;
 
+
 // ROOT headers
 #include "TObject.h"
 #include "TH1.h"
 #include "TF1.h"
 #include "TVector3.h"
 #include "TSpline.h"
+#include "TRandom3.h"
+
 // NPTool headers
 #include "TFPMWData.h"
 #include "NPCalibrationManager.h"
@@ -61,23 +64,35 @@ class TFPMWPhysics : public TObject, public NPL::VDetector {
   //////////////////////////////////////////////////////////////
   // data obtained after BuildPhysicalEvent() and stored in
   // output ROOT file
+  // NB that the order matter so if you add something do it at the end.
   public:
     vector<int>      DetectorNbr;
+    vector<int>      DetX;
+    vector<int>      DetY;
     vector<double>   PositionX;
     vector<double>   PositionY;
     vector<double>   ChargeX;
     vector<double>   ChargeY;
+    vector<double>   LinPositionX;
+    vector<double>   LinPositionY;  
     //double QXmax[4];
     //int StripXmax[4];
     //double QYmax[4];
     //int StripYmax[4];
     double Xf;
     double Yf;
+    double XfLin;
+    double YfLin;
     double Thetaf;
+    double ThetafLin;
     double Xt;
     double Yt;
+    double XtLin;
+    double YtLin;
     double Theta_in;
     double Phi_in;
+    double Theta_in_Lin;
+    double Phi_in_Lin;
 
 
   private:
@@ -88,7 +103,8 @@ class TFPMWPhysics : public TObject, public NPL::VDetector {
     vector<double>   DetPosZ;//!
     vector<vector<double>> Buffer_X_Q;//!
     vector<vector<double>> Buffer_Y_Q;//!
-    
+
+
     map<int, vector<pair<int,double>>> MapX;//!
     map<int, vector<pair<int,double>>> MapY;//!
     map<int, pair<int, double>> MaxQX;//!
@@ -96,15 +112,22 @@ class TFPMWPhysics : public TObject, public NPL::VDetector {
     map<int, double> QSumX;//!
     map<int, double> QSumY;//!
 
+    TRandom3 rand; //!
+    vector<vector<vector<double>>> EdgeBinDet; //! 
+    vector<vector<vector<double>>> NewEdgeBinDet; //!
+    bool LoadLinea = false;//! 
+                          /// A usefull method to bundle all operation to add a detector
+    void AddDetector(TVector3 Pos); 
+    void AddDetector(double R, double Theta, double Phi); 
+    void CalculateFocalPlanePosition(double Zf);
+    void CalculateTargetPosition();
 
-    /// A usefull method to bundle all operation to add a detector
-  void AddDetector(TVector3 Pos); 
-  void AddDetector(double R, double Theta, double Phi); 
-  void CalculateFocalPlanePosition(double Zf);
-  void CalculateTargetPosition();
-
-  //////////////////////////////////////////////////////////////
-  // methods inherited from the VDetector ABC class
+    double calculate_memory_usage(vector<vector<vector<double>>>& vec) ;
+    
+    
+    void LoadLineaFromFile(vector<vector<vector<double>>> &vec1, vector<vector<vector<double>>> &vec2, const string &filename);
+    //////////////////////////////////////////////////////////////
+    // methods inherited from the VDetector ABC class
   public:
     // read stream from ConfigFile to pick-up detector parameters
     void ReadConfiguration(NPL::InputParser);
@@ -146,8 +169,8 @@ class TFPMWPhysics : public TObject, public NPL::VDetector {
     double GetDetectorPositionZ(int Det) {return DetPosZ[Det];}
 
 
-  //////////////////////////////////////////////////////////////
-  // specific methods to FPMW array
+    //////////////////////////////////////////////////////////////
+    // specific methods to FPMW array
   public:
     // remove bad channels, calibrate the data and apply thresholds
     void PreTreat();
@@ -169,29 +192,37 @@ class TFPMWPhysics : public TObject, public NPL::VDetector {
     double AnalyticHyperbolicSecant(std::pair<int,double>& MaxQ, std::vector<std::pair<int, double>>& Map);
     double FittedHyperbolicSecant(std::pair<int,double>& MaxQ, std::vector<std::pair<int, double>>& Map);
 
+    //Linearise the reconstructed position if the yaml configuration file is
+    //present in the project.
+    //The yaml file should be in the configs directory and generated with
+    //the affiliated macro.
 
-  // objects are not written in the TTree
+    double LinearisePos(double X,
+        vector<double> &EdgeBin,
+        vector<double> & NewEdgeBin);
+
+    // objects are not written in the TTree
   private:
     TFPMWData*         m_EventData;        //!
     TFPMWData*         m_PreTreatedData;   //!
     TFPMWPhysics*      m_EventPhysics;     //!
 
-  // getters for raw and pre-treated data object
+    // getters for raw and pre-treated data object
   public:
     TFPMWData* GetRawData()        const {return m_EventData;}
     TFPMWData* GetPreTreatedData() const {return m_PreTreatedData;}
 
-  // parameters used in the analysis
+    // parameters used in the analysis
   private:
     double m_E_Threshold;     //!
     double m_Zf; //! Z focal plane position
     double m_GapSize;
 
-  // number of detectors
+    // number of detectors
   private:
     int m_NumberOfDetectors;  //!
 
-  // Static constructor to be passed to the Detector Factory
+    // Static constructor to be passed to the Detector Factory
   public:
     static NPL::VDetector* Construct();
 
